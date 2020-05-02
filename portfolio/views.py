7dca9cefdd54c133dbf0d69724ca8a01d1375d51
ale-mrt <alessandro.murtas1@gmail.com>
2020-05-03 00:29:44 +0200

    changed graphical side

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,7 +5,7 @@ from .models import Project
 def home(request):
     projects = Project.objects.order_by("-date")[:6]
     dropdownProjects = Project.objects.order_by("-date")[:3]
-    return render(request, "portfolio/home.html", {"projects":projects, "dropdownProjects": dropdownProjects})
+    return render(request, "portfolio/home.html", {"projects": projects, "dropdownProjects": dropdownProjects})
 
 # view dell'archivio. restituisce tutti i progetti per la visualizzazione nella pagina e gli ultimi tre per la visualizzazione nel bottone di dropdown
 def portfolio_archive(request):
@@ -15,9 +15,9 @@ def portfolio_archive(request):
 
 # view del singolo progetto. restituisce gli ultimi tre progetti per la visualizzazione nel bottone di dropdwon e il progetto ottenuto col suo id
 def project(request, project_id):
-    projects = Project.objects.order_by("-date")[:6]
+    project = get_object_or_404(Project, pk=project_id)
     dropdownProjects = Project.objects.order_by("-date")[:3]
-    return render(request, "portfolio/home.html", {"projects":projects, "dropdownProjects": dropdownProjects})
+    return render(request, "portfolio/project.html", {"project":project, "dropdownProjects": dropdownProjects})
 
 # view della biografia: restituisce la pagina della biografia
 def bio(request):
